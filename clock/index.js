0e7b75598ac2daec5f281ed15b88e25d68a9b5a7
Ryan G <rgoodbody11@gmail.com>
2023-01-09 17:11:33 -0600

    clock/index.js changed... maybe?

--- clock/index.js
+++ clock/index.js
@@ -1,6 +1,6 @@
-const hourE1 = document.querySelector(".hour")
-const minuteE1 = document.querySelector(".minute")
-const secondE1 = document.querySelector(".second")
+const hourEl = document.querySelector(".hour")
+const minuteEl = document.querySelector(".minute")
+const secondEl = document.querySelector(".second")
 
 function updateClock(){
     const currentDate = new Date();
@@ -9,11 +9,11 @@ function updateClock(){
     const minute = currentDate.getMinutes();
     const second = currentDate.getSeconds();
     const hourDeg = (hour / 12) * 360;
-    hourE1.style.transform = `rotate(${hourDeg}deg)`
+    hourEl.style.transform = `rotate(${hourDeg}deg)`
     const minuteDeg = (minute / 60) * 360;
-    minuteE1.style.transform = `rotate(${minuteDeg}deg)`
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`
     const secondDeg = (second / 60) * 360;
-    secondE1.style.transform = `rotate(${secondDeg}deg)`
+    secondEl.style.transform = `rotate(${secondDeg}deg)`
 
     //console.log(hour, minute, second);
 }
